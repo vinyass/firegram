b2980849b5fbd65bb688f5f30d138702813fae9a
Vinyas Singh <kumar.v.singh@oracle.com>
2020-08-01 04:01:53 +0530

    added delete functionality

--- src/comps/ImageGrid.js
+++ src/comps/ImageGrid.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import useFirestore from "../hooks/useFirestore";
 import { motion } from "framer-motion";
+import trashIcon from "../assets/trash.png";
+import useFirestoreDelete from "../hooks/useFirestoreDelete";
 
 const ImageGrid = ({ setSelectedImg }) => {
+  const [imgToDelete, setImgToDelete] = useState(null);
   const { docs } = useFirestore("images");
+
+  useFirestoreDelete("images", imgToDelete);
+
+  const handleImageClicked = (e, id) => {
+    if (e.target.classList.contains("img")) {
+      setSelectedImg(id);
+    }
+  };
   return (
     <div className="img-grid">
       {docs &&
@@ -13,16 +24,22 @@ const ImageGrid = ({ setSelectedImg }) => {
             layout
             className="img-wrap"
             key={doc.id}
-            onClick={() => setSelectedImg(doc.url)}
+            onClick={(e) => handleImageClicked(e, doc.url)}
           >
             <motion.img
               src={doc.url}
+              className="img"
               alt="uploaded pic"
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               transition={{ delay: 1 }}
             />
-            <button>Delete</button>
+            <img
+              className="img-delete"
+              src={trashIcon}
+              alt="delete"
+              onClick={() => setImgToDelete(doc.id)}
+            />
           </motion.div>
         ))}
     </div>
